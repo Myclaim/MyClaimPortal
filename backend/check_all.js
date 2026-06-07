1dad5a173b03d1056ae5a971907d2bd815804994
Pratham010504 <pratham010504@gmail.com>
2026-06-07 13:16:01 +0530

    Clean up temporary scripts

--- backend/check_all.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const Ticket = require('./models/Ticket');
-mongoose.connect('mongodb://127.0.0.1:27017/myclaim').then(async () => {
-  const t = await Ticket.find({});
-  console.log(t.map(x => ({ id: x._id, status: x.status, progress: x.progress })));
-  process.exit(0);
-});
